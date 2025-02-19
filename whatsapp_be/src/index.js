6bf6b23775d0ac6beb61706a8d5481f1fd8a542a
raghav <raghav_sobti@yahoo.com>
2025-02-19 13:29:58 +0400

    error handling done right
    error handling done right

--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -5,9 +5,33 @@ import logger from "./configs/logger.config.js";
 // dotenv configuration
 dotenv.config();
 
-
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-	logger.info("server is listening on port..", PORT )
-})
+let server = app.listen(PORT, () => {
+  logger.info(`server is listening on port.. ${PORT}`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    logger.info(`Server Closed.`);
+    process.exit(1);
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }
+});
