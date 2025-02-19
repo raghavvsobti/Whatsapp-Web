455eec0fe352f29f3ec9b382215189eac98b2d78
raghav <raghav_sobti@yahoo.com>
2025-02-19 13:31:07 +0400

    default error handling implemented
    default error handling implemented

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -7,49 +7,67 @@ import compression from "compression";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 
-
 const app = express();
 
 // parse json req url
 app.use(express.json());
 
 // parse json req body
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // morgan
-if (process.env.NODE_ENV !== 'production') { 
-	app.use(morgan("dev"))
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
 }
 
 // helmet
-app.use(helmet())
+app.use(helmet());
 
-// sanitize 
-app.use(mongoSanitize())
+// sanitize
+app.use(mongoSanitize());
 
 // Enable cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 // gzip Compression
-app.use(compression())
+app.use(compression());
 
 // fileUpload
-app.use(fileUpload({
-	useTempFiles: true
-}))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 
 // CORS
-app.use(cors({
-	origin: 'http://localhost:3000'
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.get("/", (req, res) => { 
-	res.send("Hello from server!")
-})
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist."));
+});
 
-app.post("/", (req, res) => { 
-	console.log(req.body);
-	res.send( req.body)
-})
+//error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 export default app;
